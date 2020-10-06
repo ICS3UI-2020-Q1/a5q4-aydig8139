afd24b916a612b0e2d2f1e02d4cf76595de10f38
aydig8139 <aydig8139@wrdsb.ca>
2020-10-06 15:31:24 +0000

    finished question to calculate interest

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 /**
- *
- * @author 
+ * calculates interest
+ * @author Rose
  */
 public class Main {
 
@@ -9,7 +11,30 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // create scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    //ask user to enter their starting balance and declare a variable to store the amount
+    System.out.println("Please enter a starting balance");
+
+    double balance = input.nextDouble();
+
+    //ask user for the interest rate and declare a variable to store it
+    System.out.println("Please enter your interest rate");
+
+    int interest = input.nextInt();
+    double interestRate = interest*0.01;
+
+    //ask user hwo long they will invest and declare a variable to store the number
+    System.out.println("Please enter the number of years you will invest for");
+
+    int years = input.nextInt();
     
+    for(int count = 1; count <= years; count++){
+      balance = balance + (balance*interestRate);
+      
+    }
+    System.out.println(balance);
+    }
   }
-}
+
